e74b145cb039f90ff47688138cb503c93606d7d0
Carina.Nuyts <nuyts.carine@gmail.com>
2017-02-28 13:25:56 +0100

    2de commit

--- springBasisTheorie/src/main/java/be/vdab/ClassA.java
+++ springBasisTheorie/src/main/java/be/vdab/ClassA.java
@@ -2,6 +2,16 @@ package be.vdab;
 
 class ClassA implements InterfaceA {
 
+	private String telefoonNrHelpDesk;
+
+	public String getTelefoonNrHelpDesk() {
+		return telefoonNrHelpDesk;
+	}
+
+	public void setTelefoonNrHelpDesk(String telefoonNrHelpDesk) {
+		this.telefoonNrHelpDesk = telefoonNrHelpDesk;
+	}
+
 	@Override
 	public String getBoodschap() {
 		return "ClassA object";
--- springBasisTheorie/src/main/java/be/vdab/Main.java
+++ springBasisTheorie/src/main/java/be/vdab/Main.java
@@ -7,8 +7,9 @@ class Main {
 	public static void main(String[] args) {
 
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("container.xml")) {
-			System.out.println(context.getBean(ClassA.class).getBoodschap());
-			System.out.println(context.getBean(InterfaceB.class).getBoodschap());
+			System.out.println(context.getBean("classA", ClassA.class).getBoodschap());
+//			System.out.println(context.getBean(InterfaceB.class).getBoodschap());
+			System.out.println(context.getBean(ClassA.class).getTelefoonNrHelpDesk());
 		}
 
 	}
